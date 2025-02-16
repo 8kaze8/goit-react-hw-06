1573eb9e581a56847145722232949a8263de488e
8kaze8 <kadir.zeyrek1@gmail.com>
2025-02-16 16:57:01 +0300

    fix: configuration files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/goit-react-hw-06/",
@@ -12,9 +11,7 @@ export default defineConfig({
     minify: true,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+        manualChunks: undefined,
       },
     },
   },
